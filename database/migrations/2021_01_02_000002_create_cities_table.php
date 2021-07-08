2a4efea2f83c5e3725f34d37251bfec3671783c1
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-07-09 01:01:25 +0700

    Update

---
+++ database/migrations/2021_01_02_000002_create_cities_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration {
+  public function up() {
+    Schema::create('cities', function (Blueprint $table) {
+      $table->increments('id');
+      $table->integer('province_id')->unsigned();
+      $table->string("name");
+      $table->string("type");
+      $table->string("postal_code");
+      $table->timestamps();
+      $table->foreign('province_id')->references('id')->on('provinces')->onDelete('restrict')->onUpdate('restrict');
+    });
+  }
+
+  public function down() {
+    Schema::dropIfExists('cities');
+  }
+}
--- database/seeds/DefaultSeeder.php
+++ database/seeds/DefaultSeeder.php
@@ -6,5 +6,7 @@ class DefaultSeeder extends Seeder {
   public function run() {
     $this->call(Accesses::class);
     $this->call(Users::class);
+    $this->call(ProvinceSeeder::class);
+    $this->call(CitySeeder::class);
   }
 }
